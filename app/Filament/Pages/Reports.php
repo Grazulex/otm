d8e4c227ef15e91f9e7244290f599d98a15684d8
jean-marc strauven <jms@grazulex.be>
2023-01-05 00:40:26 +0100

    add report

--- app/Filament/Pages/Reports.php
+++ app/Filament/Pages/Reports.php
@@ -51,7 +51,7 @@ class Reports extends Page implements Forms\Contracts\HasForms
     public function submit()
     {
         if ($this->ReportType == 'customervsitems') {
-            $plates = Plate::select(DB::raw('origin, customer, product_type, plate_type,  count(*) as total'))->whereBetween('created_at', [$this->StartDate, $this->StopDate])->groupBy('origin', 'customer', 'product_type', 'plate_type')->orderBy('origin')->orderBy('customer')->get()->toArray();
+            $plates = Plate::select(DB::raw('origin, customer, product_type, plate_type, type,  count(*) as total'))->whereBetween('created_at', [$this->StartDate, $this->StopDate])->groupBy('origin', 'customer', 'product_type', 'plate_type', 'type')->orderBy('origin')->orderBy('customer')->get()->toArray();
 
             $export = new DefaultExport($plates);
 
