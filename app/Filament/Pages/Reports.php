27dad254a87c05d60b3c1de10d50ff5dac6b3953
jean-marc strauven <jms@grazulex.be>
2023-01-05 00:36:09 +0100

    add report

--- app/Filament/Pages/Reports.php
+++ app/Filament/Pages/Reports.php
@@ -4,16 +4,15 @@ namespace App\Filament\Pages;
 
 use App\Exports\DefaultExport;
 use App\Models\Plate;
-use Filament\Pages\Page;
-use Filament\Forms;
 use Carbon\Carbon;
-use Maatwebsite\Excel\Facades\Excel;
+use Filament\Forms;
+use Filament\Pages\Page;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
 
-class Reports extends Page implements Forms\Contracts\HasForms 
+class Reports extends Page implements Forms\Contracts\HasForms
 {
-
-    use Forms\Concerns\InteractsWithForms; 
+    use Forms\Concerns\InteractsWithForms;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
@@ -24,19 +23,21 @@ class Reports extends Page implements Forms\Contracts\HasForms
     protected static ?string $navigationGroup = 'Admin';
 
     public $StartDate;
+
     public $StopDate;
+
     public $ReportType;
 
     public function mount(): void
     {
         $this->form->fill([
             'StartDate' => Carbon::now()->startOfMonth(),
-            'StopDate' => Carbon::now()
+            'StopDate' => Carbon::now(),
         ]);
     }
 
-    protected function getFormSchema(): array 
-    {   
+    protected function getFormSchema(): array
+    {
         return [
             Forms\Components\DatePicker::make('StartDate')->maxDate(Carbon::now())->required(),
             Forms\Components\DatePicker::make('StopDate')->maxDate(Carbon::now())->required(),
@@ -45,7 +46,7 @@ class Reports extends Page implements Forms\Contracts\HasForms
             ])
             ->required(),
         ];
-    } 
+    }
 
     public function submit()
     {
@@ -53,9 +54,8 @@ class Reports extends Page implements Forms\Contracts\HasForms
             $plates = Plate::select(DB::raw('origin, customer, product_type, plate_type,  count(*) as total'))->whereBetween('created_at', [$this->StartDate, $this->StopDate])->groupBy('origin', 'customer', 'product_type', 'plate_type')->orderBy('origin')->orderBy('customer')->get()->toArray();
 
             $export = new DefaultExport($plates);
-    
-            return Excel::download($export,'CustomerVsItems-'.Carbon::createFromDate($this->StartDate)->format('Ymd').'-'.Carbon::createFromDate($this->StopDate)->format('Ymd').'.xlsx', \Maatwebsite\Excel\Excel::XLSX);
+
+            return Excel::download($export, 'CustomerVsItems-'.Carbon::createFromDate($this->StartDate)->format('Ymd').'-'.Carbon::createFromDate($this->StopDate)->format('Ymd').'.xlsx', \Maatwebsite\Excel\Excel::XLSX);
         }
     }
-
 }
