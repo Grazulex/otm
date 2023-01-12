e27a1b415a1bdb6f340bd64aeab03deb8602704b
jean-marc strauven <jms@grazulex.be>
2023-01-12 01:21:10 +0100

    update repport

--- app/Filament/Pages/Reports.php
+++ app/Filament/Pages/Reports.php
@@ -51,7 +51,7 @@ class Reports extends Page implements Forms\Contracts\HasForms
     public function submit()
     {
         if ($this->ReportType == 'customervsitems') {
-            $plates = Plate::select(DB::raw('created_at, origin, customer, product_type, type, is_cod, is_rush, amount, datas->>"$.payment_method" as payment_method, datas->>"$.price" as price'))->whereBetween('created_at', [$this->StartDate, $this->StopDate])->orderBy('created_at')->orderBy('origin')->orderBy('customer')->get()->toArray();
+            $plates = Plate::select(DB::raw('created_at, origin, customer, product_type, type, is_cod, is_rush, amount, replace(json_extract(datas, "$.payment_method"),""","") as payment_method, replace(json_extract(datas, "$.price"),""","") as price'))->whereBetween('created_at', [$this->StartDate, $this->StopDate])->orderBy('created_at')->orderBy('origin')->orderBy('customer')->get()->toArray();
 
             $export = new DefaultExportHeading($plates, [
                 'Date',
